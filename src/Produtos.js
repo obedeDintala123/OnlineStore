e3d6af44e94f3a10674b0b282a72ad03084f4942
obedeDintala123 <obededintala@gmail.com>
2024-09-29 21:39:14 +0100

    Adicionando files

--- src/Produtos.js
+++ src/Produtos.js
@@ -1,14 +1,46 @@
 import "./Produtos.css";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 function Buy() {
   const produto = JSON.parse(localStorage.getItem("produtoSelecionado"));
-  const navegate = useNavigate();
+  const [quantidade, setQuantidade] = useState(1);
+  const resetButtonStyles = () => {
+    const allButtons = document.querySelectorAll(".size-button");
+    allButtons.forEach((button) => {
+      button.style.background = ""; // Remove a cor de fundo
+      button.style.color = ""; // Remove a cor do texto
+    });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    resetButtonStyles();
+    event.target.style.background = "#fff";
+    event.target.style.color = "#3a89ff";
+    console.log(quantidade * produto.preco);
+  };
+
+  const minus = () => {
+    setQuantidade((prev) => prev - 1);
+    if (quantidade <= 1) {
+      setQuantidade(1);
+    }
+  };
+
+  const plus = () => {
+    setQuantidade((prev) => prev + 1);
+  };
+
   return (
     <div className="buy-container">
-      <button className="back-button" onClick={() => {window.location.reload()}}>
-        <svg color="#fff"
+      <button
+        className="back-button"
+        onClick={() => {
+          window.location.reload();
+        }}
+      >
+        <svg
+          color="#fff"
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
@@ -34,14 +66,71 @@ function Buy() {
           <div className="size-container">
             <span>Size</span>
             <div className="size-buttons">
-              <button>S</button>
-              <button>M</button>
-              <button>L</button>
-              <button>XL</button>
-              <button>XXL</button>
+              <button
+                name="S-button"
+                className="size-button"
+                onClick={handleClick}
+              >
+                S
+              </button>
+              <button
+                name="M-button"
+                className="size-button"
+                onClick={handleClick}
+              >
+                M
+              </button>
+              <button
+                name="L-button"
+                className="size-button"
+                onClick={handleClick}
+              >
+                L
+              </button>
+              <button
+                name="XL-button"
+                className="size-button"
+                onClick={handleClick}
+              >
+                XL
+              </button>
+              <button
+                name="XXL-button"
+                className="size-button"
+                onClick={handleClick}
+              >
+                XXL
+              </button>
             </div>
           </div>
-          <span className="content2-produto-preco">{produto.preco} AOA</span>
+          <div className="preco-quantidade-content">
+            <span className="content2-produto-preco">{produto.preco} AOA</span>
+            <div className="quantidade-content">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-dash-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={minus}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z" />
+              </svg>
+              <span>{quantidade}</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={plus}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+              </svg>
+            </div>
+          </div>
           <div className="contente2-buttons">
             <button className="buyNow-button">Buy Now</button>
             <button className="add-button">Add to Cart</button>
--- src/SignIn.js
+++ src/SignIn.js
@@ -19,7 +19,6 @@ export default function SignIn() {
     e.preventDefault();
     let email = inputEmail.current.value;
     let userFound = users.find((user) => user.email === email);
-    const userInitial = localStorage.setItem("userInitial", email);
     let container = document.body;
     let div = document.createElement("div");
     div.className = "message-container";
@@ -30,6 +29,7 @@ export default function SignIn() {
     } 
     
     if (userFound) {
+      const userInitial = localStorage.setItem("userInitial", email);
       div.innerHTML =
         'Login feito com sucesso <svg style="color:green;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>';
       container.appendChild(div);
